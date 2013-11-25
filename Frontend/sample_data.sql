580d081c6aa250b0adba2c6941f7b92a46a999d3
Michal Wielgus <wielgus.michal+github@gmail.com>
2013-11-26 00:05:57 +0100

    More dummy data, fancier (but buggy, as detailed in comments) display.

--- Frontend/sample_data.sql
+++ Frontend/sample_data.sql
@@ -1,16 +1,25 @@
 insert into Users values(100001, 'mwielgus',      'wymyslcos', CURRENT_TIMESTAMP);
 insert into Users values(100002, 'jkleszczynski', 'wymyslcos', CURRENT_TIMESTAMP);
 insert into Users values(100003, 'joahum',        'wymyslcos', CURRENT_TIMESTAMP);
+insert into Users values(100004, 'euzebiusz',     'wymyslcos', CURRENT_TIMESTAMP);
 
 insert into Objects values(200001, 100001, 'Bystra',  'Najwyższy szczyt Tatr Zachodnich (2248).', null, null);
 insert into Objects values(200002, 100001, 'Błyszcz', 'Zwornik dla grani Bystrej.',               null, null);
 
-insert into Images values(300001, 100001, 'sample.jpg', 'Zdjęcie robione z okolic Beskidu/Suchej Przełęczy.', CURRENT_TIMESTAMP, null);
+insert into Images values(300001, 100001, 'sample.jpg' , 'Zdjęcie robione z okolic Beskidu/Suchej Przełęczy.', CURRENT_TIMESTAMP, null);
+insert into Images values(300002, 100001, 'sample2.jpg', 'Zdjęcie robione z okolic Świnicy.',                  CURRENT_TIMESTAMP, null);
 
 insert into POI values(400001, 300001, 100001, 635, 235);
 insert into POI values(400002, 300001, 100001, 680, 247);
+insert into POI values(400003, 300002, 100001,  52, 310);
+insert into POI values(400004, 300002, 100001, 137, 324);
 
 insert into Assumptions values(500001, 400001, 200001, 100001, CURRENT_TIMESTAMP);
 insert into Assumptions values(500002, 400002, 200002, 100001, CURRENT_TIMESTAMP);
+insert into Assumptions values(500003, 400003, 200001, 100001, CURRENT_TIMESTAMP);
+insert into Assumptions values(500004, 400004, 200002, 100001, CURRENT_TIMESTAMP);
 
-insert into Comments values(600001, 500001, 100003, 'Właśnie tak! Widać po Błyszczu.', CURRENT_TIMESTAMP, +1);
+insert into Comments values(600001, 500001, 100003, 'Właśnie tak! Widać po Błyszczu.',                     CURRENT_TIMESTAMP, +1);
+insert into Comments values(600002, 500001, 100004, 'Zgadzam się z Tobą, joahumie, po tym właśnie widać.', CURRENT_TIMESTAMP, +1);
+insert into Comments values(600003, 500004, 100004, 'Trudno orzec po tym zdjęciu - jesteś przekonany?.',   CURRENT_TIMESTAMP,  0);
+insert into Comments values(600004, 500004, 100001, 'Tak. Popatrz na grań od Bystrej na dużym zoomie.',    CURRENT_TIMESTAMP, +1);
--- Frontend/web/WEB-INF/jsp/index.jsp
+++ Frontend/web/WEB-INF/jsp/index.jsp
@@ -9,19 +9,46 @@
 	<body>
 		<p>Wipe your mind on the welcome mat and enter... THE SCARY DOOR</p>
 		<%
-		java.sql.Connection connection;
-		java.sql.ResultSet  result;
 		try {
 			// THE BEST KIND OF PASSWORDS? HARDCODED PASSWORDS!
-			connection = java.sql.DriverManager.getConnection("jdbc:derby://localhost:1527/Panoramix","panoramix","panoramix");
-			result       = connection.createStatement().executeQuery("select * from Images");
+			java.sql.Connection connection = java.sql.DriverManager.getConnection(
+				"jdbc:derby://localhost:1527/Panoramix",
+				"panoramix",
+				"panoramix"
+			);
+			java.sql.ResultSet images = connection.createStatement().executeQuery(
+				"select * from Images"
+			);
 		%>
-		<%while(result.next()) {%>
-			<%=result.getInt("iid")%>
-			<img alt="panorama" src="images/<%=result.getString("filename")%>">
+		<%while(images.next()) {%>
+		<hr>
+		<img alt="panorama" src="images/<%=images.getString("filename")%>">
+		<ul>
+			<%
+			// FIXME:
+			// the code below is stupid: it should first loop over POIs, then Assumptions, then Comments
+			// now there's no distinction between assumptions with equal oids but different pids
+			java.sql.ResultSet assumptions = connection.createStatement().executeQuery(
+				"select * from POI left join Assumptions using (pid) left join Objects using (oid) where iid = " + images.getInt("iid")
+			);
+			while(assumptions.next()) {%>
+			<li>
+				<b><%=assumptions.getString("label")%></b>: <%=assumptions.getString("description")%>
+				<ul>
+					<%
+					java.sql.ResultSet comments = connection.createStatement().executeQuery(
+						"select * from Comments join Users using (uid) where aid = " + assumptions.getInt("aid") + " order by added"
+					);
+					while(comments.next()) {%>
+					<li><b><%=comments.getString("uname")%></b>: <%=comments.getString("text")%></li>
+					<%}%>
+				</ul>
+			</li>
+			<%}%>
+		</ul>
 		<%}%>
 		<%} catch(Exception e) { %>
-			<h1> <%= e %> </h1>
+		<h1> <%= e %> </h1>
 		<%}%>
 	</body>
 </html>
