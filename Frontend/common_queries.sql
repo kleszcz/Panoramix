be18b86fe32a8ce06c5b975801b1d9c0a329ff0d
Jan Kleszczyński <9kleszcz@fatcat.ftj.agh.edu.pl>
2014-01-16 11:26:51 +0100

    Queries tests

--- Frontend/common_queries.sql
+++ Frontend/common_queries.sql
@@ -1,2 +1,4 @@
 select * from Images join POI using (iid) join Assumptions using (pid) where oid = 200001;
 select * from POI    left join Assumptions using (pid) left join Objects using (oid) where iid=300001;
+select * from POI left join Assumptions using (pid) left join Objects using (oid) where iid=300002;
+select * from (select A.*, (select coalesce(sum(vote),0) from Comments as C where C.aid = A.aid) as votes from Assumptions as A where pid = 400004) as AV left join Objects using (oid) order by votes desc, AV.added;
---
+++ Frontend/image_queries.sql
@@ -0,0 +1,1 @@
+select POI.pid, POI.x, POI.y, OBJECTS.Label, Objects.DESCRIPTION, A.Aid, (select coalesce(sum(vote),0) from comments where aid = A.aid) as votes from POI left join ASSUMPTIONS as A using(pid) left join Objects using (oid) where POI.IID = 300002 order by pid, votes asc;
