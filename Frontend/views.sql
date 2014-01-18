3d4f7e8a91dfef8bfec063400ad36f573ff1711a
Michal Wielgus <wielgus.michal+github@gmail.com>
2014-01-18 22:31:57 +0100

    Unfuck query.

--- Frontend/views.sql
+++ Frontend/views.sql
@@ -1,1 +1,7 @@
-create view votes as select DISTINCT A.OID, (select coalesce(sum(vote),0) from Comments as C where C.aid = A.aid )as votes from Assumptions as A;
+drop view Votes;
+
+create view Votes as
+	select
+		A.*,
+		(select coalesce(sum(vote),0) from Comments as C where C.aid = A.aid) as votes
+	from Assumptions as A;
